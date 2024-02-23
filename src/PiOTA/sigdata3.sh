51df35ac1f78d2708289caef12269225f894c4e1
DougLung2000 <83193741+DougLung2000@users.noreply.github.com>
2024-02-22 20:33:48 -1000

    Update sigdata3.sh to reset every 5 minutes
    Resetting the tuner is now handled in this script instead of restarting the sigdata.py program. Reset time was set to 300 seconds but easily changed by modifying the script.

--- src/PiOTA/sigdata3.sh
+++ src/PiOTA/sigdata3.sh
@@ -1,1 +1,19 @@
-dvbv5-zap -a 1 -m -I ZAP -c ~/dtvdata/channels.zap -C US 'KMEB-HD'
+#!/bin/bash
+
+int_handler()
+{
+    echo "Interrupted."
+    # Kill the parent process of the script.
+    kill $PPID
+    exit 1
+}
+dtvdata()
+{
+    trap 'int_handler' INT
+    timeout --foreground 300 dvbv5-zap -a 1 -m -I ZAP -c ~/dtvdata/channels.zap -C US 'KMEB-HD'
+}
+
+while true; do
+    dtvdata
+    sleep 15
+done
