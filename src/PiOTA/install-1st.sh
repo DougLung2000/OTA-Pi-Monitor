4e7fddf6634373cc21db993ffe3ee0dee3c8ae5c
DougLung2000 <83193741+DougLung2000@users.noreply.github.com>
2026-06-24 21:45:34 -1000

    Install script for default OTA-Pi-Monitor
    This script will install the programs and files necessary to run OTA-Pi-Monitor. Scripts referring to the channel and programs to be monitored and the labels in index.html file will have to be edited to match the station and parameters being monitored. Additional scripts will be provided for that purpose.

---
+++ src/PiOTA/install-1st.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Installing packages required for OTA Pi Monitor - This may take a while"
+echo "Enter system password if requested to install system files"
+sudo apt install dvb-tools ffmpeg nano w-scan git
+mkdir ~/dtvdata
+python ~m venv ~/dtvdata/venv
+cp *.sh ~/dtvdata/
+chmod 755 ~/dtvdata/*.sh
+cp channels.zap ~/dtvdata/
+cp sigdata-guni2.py ~/dtvdata/venv/
+cp requirements.txt ~/dtvdata/venv/
+cp -R static ~/dtvdata/venv/
+cp -R templates ~/dtvdata/venv/
+sudo dpkg -i tsduck_3.37-3636.debian12_arm64.deb
+sudo cp w_scan2 /usr/local/bin
+sudo cp systemd/* /etc/systemd/system/
+cd ~/dtvdata/venv
+/home/tv/dtvdata/venv/bin/pip3 install -r requirements.txt
+echo "If no errors occurred during installation, connect tuner"
+echo "and antenna to allow scanning channels."
